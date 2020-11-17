5f38852711e9b15646bcf964233c5b9f3283f976
ZingerMountainMelt <71338371+ZingerMountainMelt@users.noreply.github.com>
2020-11-17 13:44:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const api_url = "https://script.google.com/macros/s/AKfycbzehsYi8UZ0p-GPvrbS9IDaMq4Z4JAaGc44nEDYbMa3b1nsIFOG/exec";
 const animg = "https://drive.google.com/uc?id=1B_X5ik7PwJbenk8MZo958ca4INq0VG8d";
-//download file to object
-//jquery and json node & express
-
-//"https://teachablemachine.withgoogle.com/models/x7j9-fYZd/model.json"
 
 const { ml5 } = window;
 const classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/x7j9-fYZd/model.json", console.log);
@@ -12,10 +8,8 @@ const result = document.querySelector(".result h2");
 const image = document.querySelector(".image");
 const response = document.querySelector(".response h4");
 
-window.addEventListener('DOMContentLoaded', addImages)
+window.addEventListener('DOMContentLoaded',addImages)
 const output = document.querySelector('.output');
-
-var GV_image1_url = null;
 
 function addImages(){
   fetch(api_url).then(function(rep) {
@@ -27,37 +21,27 @@ function addImages(){
       console.log(url);
       console.log(id);
       console.log(file_name);
-      GV_image1_url = url;
-      console.log(GV_image1_url);
-      document.getElementById('image1').src = URL.createObjectURL(url);
-      //document.getElementById('image1').src = url;
+      let imgURL = url;
+      console.log(imgURL);
+      document.getElementById('url').textContent = url;
       
-      document.getElementById('spanImage1_url').innerText = url;
-      //let img = document.createElement('img');
-      //img.setAttribute('src',val.url);
-      //output.appendChild(img);
-      //console.log(img);
+      let img = document.createElement('img');
+      img.setAttribute('src',val.url);
+      output.appendChild(img);
+      console.log(img);
       
-      //classifyImage(url);
-      //classifyImage("https://cdn.mos.cms.futurecdn.net/QjuZKXnkLQgsYsL98uhL9X.jpg");
+      classifyImage("https://cdn.mos.cms.futurecdn.net/QjuZKXnkLQgsYsL98uhL9X.jpg");
     })
   })
 }
 
-function showImage1url() {
-  alert(GV_image1_url);
-}
-
-console.log(GV_image1_url);
-
 console.log("Pre-Classifier");
 
 async function classifyImage() {
-  const image = document.querySelector(".image");
-  const results = await classifier.classify();
+  const results = await classifier.classify(image);
   result.innerText = results[0].label;
   console.log("Classification Start");
-    alert();
+  
     if (result.innerText == "Chaplin") {
       chaplin();
     }
@@ -77,7 +61,7 @@ async function classifyImage() {
 
 function sendEmailO(){
         console.log("Oakley Email Start");
-        console.log(GV_image1_url);
+        console.log(animg);
   
         Email.send({
           SecureToken: "8e5f49d9-d5f7-4c8c-a4ba-f491fd062ae9",
@@ -88,14 +72,14 @@ function sendEmailO(){
           Attachments : [
 	          {
 		          name : "Oakley.jpg",
-		          path : GV_image1_url
+		          path : animg
 	          }]
         });
 }
       
 function sendEmailC(){
         console.log("Chaplin Email Start");
-        console.log(GV_image1_url);
+        console.log(animg);
   
         Email.send({
           SecureToken: "8e5f49d9-d5f7-4c8c-a4ba-f491fd062ae9",
@@ -106,7 +90,7 @@ function sendEmailC(){
           Attachments : [
 	          {
 		          name : "Chaplin.jpg",
-		          path : GV_image1_url
+		          path : animg
 	          }]
         });
 }
@@ -114,13 +98,13 @@ function sendEmailC(){
 function oakley(){
   console.log("Oakley!");
   response.innerText = "Good girl! :)";
-  //sendEmailO();
+  sendEmailO();
 }
 
 function chaplin() {
   console.log("Chaplin!");
   response.innerText = "Bad boy! >:(";
-  //sendEmailC();
+  sendEmailC();
 }
 
 function nothing() {
@@ -128,11 +112,7 @@ function nothing() {
   response.innerText = "Must've been the wind...";
 }
 
-setTimeout(() => {  classifyImage(GV_image1_url); }, 10000);
-
-setTimeout(() => {  chaplin(); }, 10000);
-
-//function handleUpload(files) {
-  //image.src = "https://cdn.mos.cms.futurecdn.net/QjuZKXnkLQgsYsL98uhL9X.jpg";
-  //setTimeout(classifyImage, 50);
-//}
+function handleUpload(files) {
+  image.src = "https://cdn.mos.cms.futurecdn.net/QjuZKXnkLQgsYsL98uhL9X.jpg";
+  setTimeout(classifyImage, 50);
+}
